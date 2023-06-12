b1aeef531d7b3001aa114b3abcf511d1b697db40
IslombekOrifov <islombekorifov0199@gmail.com>
2023-06-12 15:19:38 +0500

    changed apps directory

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts.apps.AccountsConfig',
+    'src.v1.accounts.apps.AccountsConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
